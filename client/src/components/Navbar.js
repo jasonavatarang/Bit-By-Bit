92975d4e5311318ec5b7b98b45e9723f7762728e
aranguizdiasc <aranguizdiasc@ufl.edu>
2024-06-29 13:49:01 -0400

    Fixed navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,27 +1,22 @@
 // src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../logo.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Navbar</Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-            <Link className="nav-link" to="/about">About</Link>
+        <Link className="navbar-brand" to="/">
+          <img src={logo} alt="" width="27" height="27" class="d-inline-block align-text-top" /> Bit by Bit
+        </Link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+              <Link className="nav-link" to="/about">About</Link>
           </div>
         </div>
       </div>
